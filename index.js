b61a81476909e88c07b6b44f04450004a44d479f
Toster <ebebebeawdawdaw@gmail.com>
2025-11-18 11:16:01 +0100

    4_1.1 poprawionee

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // define endpoint for exercise 1 here
 app.get("/math/circle/:r", (req, res) => {
-  const { r } = req.query;
+  const { r } = req.params;
 
   // check for required parameters
   if (!r) {
@@ -26,6 +26,32 @@ app.get("/math/circle/:r", (req, res) => {
 });
 
 //TODO2
+// define endpoint for exercise 1 here
+app.get("/math/rectangle/:width/:height", (req, res) => {
+  const { width, height } = req.params;
+
+  // check for required parameters
+  if (!width || !height) {
+    const missingParams = [];
+    if (!width) {
+      missingParams.push("w");
+    }
+    if (!height) {
+      missingParams.push("h");
+    }
+    const errorMessage = `Missing Required GET parameters: ${missingParams.join(
+      ", "
+    )}`;
+    return res.status(400).send(errorMessage);
+  }
+
+  // send greeting
+  const message = `area:${width * height}, circumference ${
+    2 * width + 2 * height
+  }`;
+  res.type("text").send(message);
+  res.json(result);
+});
 
 //TODO3
 
