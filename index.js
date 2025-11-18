b22dba0c0250cc837716c6d7c56e041a4bc06be7
Toster <ebebebeawdawdaw@gmail.com>
2025-11-18 10:45:07 +0100

    k

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // define endpoint for exercise 1 here
-app.get('/math/circle/:r', (req, res) => {
-//TODO1  
+app.get("/math/circle/:r", (req, res) => {
+  //TODO1
   res.json(result);
 });
 
 //TODO2
 
-
 //TODO3
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
